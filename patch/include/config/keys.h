e3a212ee3d52c5a61266e59ab56ada275d7c6d95
WANDEX <15724752+WANDEX@users.noreply.github.com>
2021-09-29 16:18:46 +0300

    [keys.h] remove Ctrl+l mapping -> as it's makes it unavailable in vim

--- patch/include/config/keys.h
+++ patch/include/config/keys.h
@@ -49,6 +49,5 @@ static Shortcut shortcuts[] = {
 	{ CSHIFT,               XK_N,           historyPageY,   {.i =  1} },
 	{ CSHIFT,               XK_E,           historyPageY,   {.i = -1} },
 	{ MODKEY,               XK_k,           historyQuit,    {.i =  0} },
-	{ ControlMask,          XK_l,           historyQuit,    {.i =  0} },
 	{ MODKEY,               XK_a,           normalMode,     {.i =  0} },
 };
